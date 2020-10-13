be18dc553e655c3adf57838445a86e7e417a57d2
risonakamo <hmykanae@gmail.com>
2020-10-13 16:55:59 -0400

    hw6 input checking functionality

--- hw6/js/app.js
+++ hw6/js/app.js
@@ -1,3 +1,39 @@
 (()=>{
-    angular.module("LunchCheck",[]);
+    const app=angular.module("LunchCheck",[]);
+    app.controller("LunchCheckController",LunchCheckController);
+
+    LunchCheckController.$inject=["$scope"];
+    function LunchCheckController($scope)
+    {
+        $scope.lunchInputValue="";
+        $scope.lunchMessage="";
+
+        $scope.buttonClick=buttonClick;
+
+        function buttonClick()
+        {
+            var numLunch=$scope.lunchInputValue.split(",").filter((x)=>{
+                return x.trim().length;
+            });
+            console.log(numLunch);
+
+            numLunch=numLunch.length;
+
+            if (!numLunch)
+            {
+                $scope.lunchMessage="Please enter data first";
+                return;
+            }
+
+            else if (numLunch<=3)
+            {
+                $scope.lunchMessage="Enjoy!";
+            }
+
+            else
+            {
+                $scope.lunchMessage="Too much!";
+            }
+        }
+    }
 })();
