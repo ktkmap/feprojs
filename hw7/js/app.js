06ce655f1c459b734b9f7f353423a7f2e3780267
risonakamo <hmykanae@gmail.com>
2020-10-27 18:25:04 -0400

    hw7 progress

--- hw7/js/app.js
+++ hw7/js/app.js
@@ -7,45 +7,51 @@
     ToBuyController.$inject=["ShoppingListCheckOffService"];
     function ToBuyController(shoppingListService)
     {
-
+        this.buyList=shoppingListService.buyList;
+        this.buyItem=shoppingListService.buyItem;
     }
 
     AlreadyBoughtController.$inject=["ShoppingListCheckOffService"];
     function AlreadyBoughtController(shoppingListService)
     {
-        this.hello=shoppingListService.hey;
+        this.boughtList=shoppingListService.boughtList;
     }
 
     function ShoppingListCheckOffService()
     {
-        this.hey=3;
         this.buyList=[
             {
-                name:"cookies",
+                item:"cookies",
                 count:10,
                 pricePerItem:2.20
             },
             {
-                name:"cookies2",
+                item:"cookies2",
                 count:124,
                 pricePerItem:12.20
             },
             {
-                name:"cookies3",
+                item:"cookies3",
                 count:1,
                 pricePerItem:52.12
             },
             {
-                name:"cookies4",
+                item:"cookies4",
                 count:1330,
                 pricePerItem:75.60
             },
             {
-                name:"cookies5",
+                item:"cookies5",
                 count:561,
                 pricePerItem:92.88
             }
         ];
+
         this.boughtList=[];
+
+        this.buyItem=(item)=>{
+            this.buyList.splice(this.buyList.indexOf(item),1);
+            this.boughtList.push(item);
+        };
     }
 })();
