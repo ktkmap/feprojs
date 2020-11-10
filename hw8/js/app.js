ce94e50b2d1c47ddc5fd5c20652940c2391648d5
risonakamo <hmykanae@gmail.com>
2020-11-10 18:24:40 -0500

    trying out things

--- hw8/js/app.js
+++ hw8/js/app.js
@@ -1,3 +1,22 @@
 (()=>{
     const app=angular.module("NarrowItDownApp",[]);
+    app.controller("NarrowItDownController",NarrowItDownController);
+    app.service("MenuSearchService",MenuSearchService);
+
+    NarrowItDownController.$inject=["MenuSearchService"];
+    function NarrowItDownController(menuSearchService)
+    {
+        menuSearchService.getMatchedMenuItems();
+    }
+
+    MenuSearchService.$inject=["$http"];
+    function MenuSearchService($http)
+    {
+        this.getMatchedMenuItems=async (searchTerm)=>{
+            console.log("hey");
+            console.log((await $http({
+                url:"https://davids-restaurant.herokuapp.com/menu_items.json"
+            })).data);
+        };
+    }
 })();
