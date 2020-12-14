a7f0e17af6c008d114674b717f7042d8fa21b4a7
risonakamo <hmykanae@gmail.com>
2020-12-14 01:58:30 -0500

    fix api tried making signup page

--- hw10/src/public/public.routes.js
+++ hw10/src/public/public.routes.js
@@ -40,6 +40,10 @@ function routeConfig ($stateProvider) {
           return MenuService.getMenuItems($stateParams.category);
         }]
       }
+    })
+    .state("public.signup",{
+      url:"/signup",
+      templateUrl:"src/public/sign-up/sign-up.html"
     });
 }
 })();
